db009a8b38d8227ece8886659395b3cde28e7aa2
SamserAliKhan <sasmeralikhan0448@gmail.com>
2023-03-19 19:24:42 +0530

    new changes is made

---
+++ Alexa pro1/Untitled-1.js
@@ -0,0 +1,12 @@
+// console.log("hellow");
+// name='Samser'// in js we dont need to mention the data type and String data typs are enclosed in single couts ''
+// console.log(name);
+// Sentance='This is a Sentance withe spaces';
+// console.log(Sentance);
+//tip generation
+food=prompt('What was the total amount');
+tip=prompt('Enter the tip %')/100;
+tipam=food*tip;
+tot=tipam+food;
+alert(tipam);
+console.log(tot);
